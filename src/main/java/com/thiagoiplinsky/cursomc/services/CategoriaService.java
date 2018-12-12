4deabc6197e91ecb11987126a45b93071763feaf
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2018-12-12 10:06:30 -0200

    Repository e Service criados

---
+++ src/main/java/com/thiagoiplinsky/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.thiagoiplinsky.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagoiplinsky.cursomc.domain.Categoria;
+import com.thiagoiplinsky.cursomc.resource.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+}
