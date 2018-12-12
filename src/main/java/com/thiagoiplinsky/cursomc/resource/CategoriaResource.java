81b7e7a27544dddc988ebb963f07356c54b075da
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2018-12-12 00:28:52 -0200

    Testando a primeira classe de dominio - categoria

---
+++ src/main/java/com/thiagoiplinsky/cursomc/resource/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.thiagoiplinsky.cursomc.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thiagoiplinsky.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(1, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
+	}
+}
