81a282e3cdb8a6a1f2bb01396083e868e5670ddd
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2018-12-16 14:02:23 -0200

    Listando todas as categorias com DTO

--- src/main/java/com/thiagoiplinsky/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/thiagoiplinsky/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.thiagoiplinsky.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagoiplinsky.cursomc.domain.Categoria;
+import com.thiagoiplinsky.cursomc.dto.CategoriaDTO;
 import com.thiagoiplinsky.cursomc.services.CategoriaService;
 
 @RestController
@@ -21,11 +24,15 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
+//	Endpoint para resgatar um elemento
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);		
 	}
+
+//  Endpoint para incluir um elemento
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { // RequestBody converte o Json para objeto Java
@@ -34,16 +41,29 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+//	Endpoint para alterar um elemento
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-	}
+	}	
+	
+//	Endpoint para deletar um elemento
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+//	Endpoint DTO para resgatar apenas as categorias
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll(); // o recurso (stream) percorre a lista --- (map) realiza uma operação para cada elemento da lista 
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);		
 	}
 }
