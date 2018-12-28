aa219120c49695e525aaf3f514c2304b92019615
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2018-12-28 17:42:13 -0200

    Configuração inicial do Spring Security

---
+++ src/main/java/com/thiagoiplinsky/cursomc/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.thiagoiplinsky.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+
+//	Método que indica quais os caminhos serão liberados para acesso
+	private static final String[] PUBLIC_MATCHERS = { "h2-console/**" };
+
+//	Ao utilizar o GET para produtos, é necessário passar um parâmetro para indicar de qual categoria serão exibidos os produtos
+	private static final String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+//		Configuração para acessar o banco de dados H2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}	
+
+//		Desabilita a proteção à CSRF ____ // O método cors é acionado caso haja um método CorsConfigurationSource na classe
+		http.cors().and().csrf().disable(); 
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // indica que os itens na lista PUBLIC_MATCHERS_GET serão apenas resgatados com o GET e não poderão ser alterados.
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest().authenticated();
+//		configuração que assegura que o backend não vai criar uma sessão de usuário
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+}
