a6cc1b6487fb2dfc7f5e9d0f0fbd3b57d9731097
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2019-02-07 18:29:09 -0200

    Enviando imagem via endpoint

--- src/main/java/com/thiagoiplinsky/cursomc/resource/ClienteResource.java
+++ src/main/java/com/thiagoiplinsky/cursomc/resource/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.thiagoiplinsky.cursomc.domain.Cliente;
@@ -100,5 +101,13 @@ public class ClienteResource {
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 		
+	}
+	
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadFilePicture(@RequestParam(name="file") MultipartFile file) {
+		
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build(); 
+//		Método para criação de conteúdo informando 201 created
 	}
 }
--- src/main/java/com/thiagoiplinsky/cursomc/services/ClienteService.java
+++ src/main/java/com/thiagoiplinsky/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.thiagoiplinsky.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.thiagoiplinsky.cursomc.domain.Cidade;
 import com.thiagoiplinsky.cursomc.domain.Cliente;
@@ -36,6 +38,9 @@ public class ClienteService {
 		
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -115,5 +120,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail()); 
+	}
+	
+//	Aciona a classe s3Service para upload de foto de perfil
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
