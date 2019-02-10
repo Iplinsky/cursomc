153c74ac3337ae5cce107bc859cbb5b0051d0e13
Thiago Iplinsky <thiago.iplinsky19@hotmail.com>
2019-02-10 15:19:15 -0200

    Padronizando formato das exceções

--- src/main/java/com/thiagoiplinsky/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/thiagoiplinsky/cursomc/repositories/CidadeRepository.java
@@ -2,8 +2,8 @@ package com.thiagoiplinsky.cursomc.repositories;
 
 import java.util.List;
 
-import org.springframework.data.couchbase.core.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +15,5 @@ public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
 
 	@Transactional(readOnly = true)
 	@Query("SELECT obj FROM Cidade obj WHERE obj.estado.id = :estadoId ORDER BY obj.nome")
-	public List<Cidade> findCidades(@Param("estadoId") Integer estadoId);
+	public List<Cidade> findCidades(@Param("estadoId") Integer estado_Id);
 }
--- src/main/java/com/thiagoiplinsky/cursomc/resources/exceptions/ValidationError.java
+++ src/main/java/com/thiagoiplinsky/cursomc/resources/exceptions/ValidationError.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 public class ValidationError extends StandardError {
 	private static final long serialVersionUID = 1L;
-	
-	private List<FieldMessage> erros = new ArrayList<>();   // Erro de validação com uma lista de mensagem
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);		
+
+	private List<FieldMessage> erros = new ArrayList<>(); // Erro de validação com uma lista de mensagem
+
+	public ValidationError(Long timeStamp, Integer status, String error, String message, String path) {
+		super(timeStamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErros() {
